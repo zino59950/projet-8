98a2a9fa08a9617aadb24a89b6d96b9445fd727b
zinedine dref <zinedinedref20@gmail.com>
2023-12-26 13:00:42 +0100

    css

--- src/components/Titleloc.js
+++ src/components/Titleloc.js
@@ -3,17 +3,17 @@
 import React from 'react';
 import './Titleloc.css';
 
-const Titleloc = ({ title, location, tags, description, equipments, host, rating }) => {
+const Titleloc = ({ title, location, tags, host, rating }) => {
   const { name: hostName, picture: hostPicture } = host;
 
   // Fonction pour générer les étoiles en fonction de la note
   const generateStars = () => {
     const stars = [];
     for (let i = 0; i < 5; i++) {
-      if (i< rating){
-        stars.push(<span key={i}>&#9733;</span>); // Utilisez le code HTML de l'étoile ici
+      if (i < rating) {
+        stars.push(<span key={i}>&#9733;</span>);
       } else {
-        stars.push(<span className='inactive' key={i}>&#9733;</span>); // Utilisez le code HTML de l'étoile ici
+        stars.push(<span className='inactive' key={i}>&#9733;</span>);
       }
     }
     return stars;
@@ -21,24 +21,28 @@ const Titleloc = ({ title, location, tags, description, equipments, host, rating
 
   return (
     <div className="title-loc">
-      <h1>{title}</h1>
-      <p>{location}</p>
-      <br />
-      <ul>
-        {tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-      <br />
-      {/* Ajout de l'affichage du nom et de l'image de l'hôte */}
-      <div className="host-info">
-        <p>{hostName}</p>
-        <img src={hostPicture} alt={`${hostName}'s profile`} />
+      <div className="title-info">
+        <h1>{title}</h1>
+        <p>{location}</p>
+        <br />
+        <ul>
+          {tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
 
-      {/* Ajout de l'affichage du rating sous forme d'étoiles */}
-      <div className="rating-info">
-        <p>{generateStars()}</p>
+      <div className="host-rating-info">
+        {/* Ajout de l'affichage du nom et de l'image de l'hôte */}
+        <div className="host-info">
+          <p>{hostName}</p>
+          <img src={hostPicture} alt={`${hostName}'s profile`} />
+        </div>
+
+        {/* Ajout de l'affichage du rating sous forme d'étoiles */}
+        <div className="rating-info">
+          <p>{generateStars()}</p>
+        </div>
       </div>
     </div>
   );
