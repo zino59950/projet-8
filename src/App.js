e062c03429eddb5bd842ef1b3486a44126e8922d
zinedine <zinedinedref20@gmail.com>
2024-01-12 13:42:47 +0100

    bug commentaire

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ const App = () => {
         {/* Route pour la gestion des erreurs 404 */}
         <Route path='*' element={<Error404 />} />
         {/* Route pour le composant Card - affiche la liste de logements */}
-        {/* Note : Cette route est actuellement dupliquée, vérifiez si vous souhaitez l'inclure ici */}
         <Route path='/' element={<Card />} />
       </Routes>
     </Router>
